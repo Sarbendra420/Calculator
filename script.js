926cf8ea6fe24daaaeef5efd7af457b0b4cf23af
Sarbendra Pandey <289166861+Sarbendra420@users.noreply.github.com>
2026-08-13 10:49:11 +0545

    Prepare the boiler plate and basic math functions - Create a JavaScript file and write basic math operation functions - Create a CSS file - Create a HTML file and include both external CSS and JavaScript into it

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 15));
+
+function subtract(a, b) {
+    return a - b;
+}
+
+
+function multiply(a, b) {
+    return a * b;
+}
+
+
+function divide(a, b) {
+    return a / b;
+}
