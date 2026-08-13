a9ba1892610a177a6622acf42c95ff779b824dfe
Sarbendra Pandey <289166861+Sarbendra420@users.noreply.github.com>
2026-08-13 11:13:28 +0545

    Add calculator variables and operate logic - Declare firstNumber, secondNumber, and operator variables - Implement operate function to execute math based on passed parameters

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function add(a, b) {
     return a + b;
 }
-console.log(add(5, 15));
+
 
 function subtract(a, b) {
     return a - b;
@@ -17,3 +17,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+
+function operate(operation, num1, num2) {
+    if (operation === "+") {
+        return add(num1, num2);
+    } else if (operation === "-") {
+        return subtract(num1, num2);
+    } else if (operation === "*") {
+        return multiply(num1, num2);
+    } else if (operation === "/") {
+        return divide(num1, num2);
+    }
+}
+
+console.log(operate("-", 50, 2));
